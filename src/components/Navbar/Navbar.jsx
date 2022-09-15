d490ba8cf956b28238cda1e5a5ccebb51e20c693
UbuntuLex <alexandermaniwang99@gmail.com>
2022-09-14 21:51:58 -0400

    add navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <div>Projects</div>
+            <div>Users</div>
+        </nav>
+    )
+}
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
+import Navbar from '../../components/Navbar/Navbar'
 import Landing from '../Landing/Landing'
 import './App.css'
 
 function App() {
   return (
     <>
+      <Navbar />
       {/* routes */}
       <Routes>
         <Route path='/' element={<Landing />} />
