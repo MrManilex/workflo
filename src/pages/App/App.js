89168f359b6538e054f68bf3d27fd90994df22f2
UbuntuLex <alexandermaniwang99@gmail.com>
2022-09-22 11:43:13 -0400

    connect to supabase client

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,8 +2,12 @@ import { Route, Routes } from 'react-router-dom'
 import Navbar from '../../components/Navbar/Navbar'
 import Landing from '../Landing/Landing'
 import './App.css'
+import supabase from '../../config/supabaseClient'
 
 function App() {
+
+  console.log(supabase)
+
   return (
     <>
       <Navbar />
