391b0c30d416fa1684e7eeed889e0d60ce2dcdb7
UbuntuLex <alexandermaniwang99@gmail.com>
2022-09-22 12:50:14 -0400

    remove link from projects and add to navbar title

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ export default function Navbar() {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link to='/'>Projects</Link></li>
+                        <li><Link>Projects</Link></li>
                         <li><Link to='/tickets'>Tickets</Link></li>
                         <li tabIndex={0}>
                             <Link className="justify-between">
@@ -24,7 +24,7 @@ export default function Navbar() {
                         </li>
                     </ul>
                 </div>
-                <Link className="btn btn-ghost normal-case text-xl">WorkFlo</Link>
+                <Link to='/' className="btn btn-ghost normal-case text-xl">WorkFlo</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
