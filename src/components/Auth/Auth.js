5c7e0a22c930ba6dc3a36fa54db9345ece84eeb1
UbuntuLex <alexandermaniwang99@gmail.com>
2022-09-24 12:26:11 -0400

    stub login link

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -32,31 +32,35 @@ export default function Auth() {
                     <p className="text-3xl m-5">Loading</p>
                 </>
                 :
-                <form onSubmit={handleSignup} className='text-center flex-col'>
-                    <h1 className="text-4xl m-5">Sign Up</h1>
-                    {error && <p>{error}</p>}
-                    <div>
-                        <input type="email"
-                            name="email"
-                            className="input input-bordered w-full max-w-xs my-5"
-                            placeholder="your@email.com"
-                            id="website"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <input type="password"
-                            name="password"
-                            className="input input-bordered w-full max-w-xs mb-8"
-                            placeholder="password"
-                            id="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                    <button className="btn btn-info">Sign Up</button>
-                </form>
+                <>
+                    <form onSubmit={handleSignup} className='text-center flex-col'>
+                        <h1 className="text-4xl m-5">Sign Up</h1>
+                        {error && <p>{error}</p>}
+                        <div>
+                            <input type="email"
+                                name="email"
+                                className="input input-bordered w-full max-w-xs my-5"
+                                placeholder="your@email.com"
+                                id="website"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <div>
+                            <input type="password"
+                                name="password"
+                                className="input input-bordered w-full max-w-xs mb-8"
+                                placeholder="password"
+                                id="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <button className="btn btn-info">Sign Up</button>
+                    </form>
+                    <button className="btn btn-ghost mt-4">Login</button>
+                </>
+                
             }
         </div>
     )
